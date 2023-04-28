f9139a34ea233b4b236eca59dea3bf76378fd548
Thoriq Daffa <mnurula.nu246@gmail.com>
2023-04-28 22:56:03 +0700

    additional revision to run on cloud

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// static files in the public directory
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve the index.html file at the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
